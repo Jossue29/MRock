5e4a30892cbbe3415759e4527a596e08d47d473b
Bladimir <jossuebgonzalez18@gmail.com>
2026-02-04 21:28:34 -0600

    data table

--- fabrick_inventory/inventory/views.py
+++ fabrick_inventory/inventory/views.py
@@ -1,3 +1,15 @@
+from django.http import JsonResponse
+from .models import GlobalInventory
 from django.shortcuts import render
 
-# Create your views here.
+def test_inventory(request):
+    data = list(
+        GlobalInventory.objects.all().values()[:10]
+    )
+    return JsonResponse(data, safe=False)
+
+def inventory_list(request):
+    inventory = GlobalInventory.objects.all()[:100]
+    return render(request, 'inventory/list.html', {
+        'inventory': inventory
+    })
